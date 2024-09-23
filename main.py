affa0a5710e5780cb1a57deb0bd6c6165dfbdc22
kapoor98ak <kapoor98.ak@gmail.com>
2024-09-23 09:59:08 -0300

    Create main, server, test client. Send a Correlation ID from client and get a response back in Kafka Wire Protocol - first 4 bytes are the message length and next 4 are the message itself, that is the Correlation ID here

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+#main.py
 
-# Press ⌃F5 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from server import (start_server)
+
+def main():
+    print("Starting Kafka server...")
+    HOSTNAME, PORT = "127.0.0.1", 8080
+    start_server(HOSTNAME, PORT)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -1,17 +1,41 @@
 import socket
 
-HOSTNAME = "127.0.0.1"
-PORT = 8080
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOSTNAME, PORT))
-    s.listen()
 
-    client_conn, client_addr = s.accept()
+def handle_response(request):
+    id_bytes = (request).to_bytes(4, byteorder="big")
+    return len(id_bytes).to_bytes(4, byteorder="big") + id_bytes
 
-    with client_conn:
-        print("Connected by", client_addr)
-        while True:
-            data = client_conn.recv(1024)
-            if not data:
-                break
-            client_conn.sendall(data)
+
+def handle_client(client):
+    request = client.recv(1024)
+    # response = handle_response(request)
+    response = handle_response(7)
+    client.sendall(response)
+    client.close()
+
+
+def start_server(HOSTNAME = '127.0.0.1', PORT = 8080):
+    # Create a server socket
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOSTNAME, PORT))
+    server.listen()
+    print('Server is listening on port {}'.format(PORT))
+
+    client_socket, client_add = server.accept()
+
+    while True:
+        handle_client(client_socket)
+
+    # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    #     s.bind((HOSTNAME, PORT))
+    #     s.listen()
+    #
+    #     client_conn, client_addr = s.accept()
+    #
+    #     with client_conn:
+    #         print("Connected by", client_addr)
+    #         while True:
+    #             data = client_conn.recv(1024)
+    #             if not data:
+    #                 break
+    #             client_conn.sendall(data)
